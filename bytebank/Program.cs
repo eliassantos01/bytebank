e7c166a63bfc2302d5f23bd91ae4f2c1098d00b6
eliassantos01 <eliasdossantos2010@gmail.com>
2022-11-09 21:50:32 -0300

    Adicionar arquivos de projeto.

---
+++ bytebank/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace bytebank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoAndre = new ContaCorrente();
+            contaDoAndre.titular = "André Silva";
+            contaDoAndre.numeroAgencia = 15;
+            contaDoAndre.conta = "1010-X";
+            contaDoAndre.saldo = 100;
+
+            ContaCorrente contaDaMaria = new ContaCorrente();
+            contaDaMaria.titular = "Maria Souza";
+            contaDaMaria.numeroAgencia = 17;
+            contaDaMaria.conta = "1010-5";
+            contaDaMaria.saldo = 350;
+
+            Console.WriteLine("Saldo da Conta da Maria =" + contaDaMaria.saldo);
+
+            contaDoAndre.Transferir(500, contaDaMaria);
+
+            Console.WriteLine("Saldo da Conta da André =" + contaDoAndre.saldo);
+            Console.WriteLine("Saldo da Conta da Maria =" + contaDaMaria.saldo);
+        }
+    }
+}
